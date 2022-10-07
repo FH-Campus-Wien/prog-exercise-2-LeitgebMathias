81fc0c439fe00ea16755100a4e013917bf03cb27
LeitgebMathias <mathias.leitgeb@stud.fh-campuswien.ac.at>
2022-10-07 19:38:41 +0200

    Solved Problem 1 and 2

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -7,16 +7,49 @@ public class App {
     //todo Task 1
     public void largestNumber(){
         // input your solution here
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Number 1: ");
+        double MaxNumber = scanner.nextDouble();
+        double Number;
+        if(MaxNumber <= 0){
+            System.out.println("No number entered.");
+        } else {
+            int i=2;
+            do {
+                System.out.print("Number " + i + ": ");
+                Number = scanner.nextDouble();
+                if (Number > 0){
+                    if(Number > MaxNumber) MaxNumber = Number;
+                    i++;
+                }
+            } while (Number > 0);
+            System.out.println("The largest number is " + String.format("%.2f",MaxNumber));
+        }
     }
 
     //todo Task 2
     public void stairs(){
         // input your solution here
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("n: ");
+        int NumberOfLines = scanner.nextInt();
+        if(NumberOfLines > 0) {
+            int currentNumber = 1;
+            for (int i = 1; i <=NumberOfLines; i++) {
+                for (int j = 0; j < i; j++) {
+                    if (j == i - 1) {
+                        System.out.println(currentNumber + " ");
+                    } else System.out.print(currentNumber + " ");
+                    currentNumber++;
+                }
+            }
+        } else System.out.println("Invalid number!");
     }
 
     //todo Task 3
     public void printPyramid(){
         // input your solution here
+
     }
 
     //todo Task 4
@@ -37,13 +70,13 @@ public class App {
     public static void main(String[] args){
         App exercise2 = new App();
 
-        System.out.println("Task 1: Largest Number");
-        exercise2.largestNumber();
+        //System.out.println("Task 1: Largest Number");
+        //exercise2.largestNumber();
 
         System.out.println("\nTask 2: Stairs");
         exercise2.stairs();
 
-        System.out.println("\nTask 3: Pyramide");
+        /*System.out.println("\nTask 3: Pyramide");
         exercise2.printPyramid();
 
         System.out.println("\nTask 4: Raute");
@@ -53,6 +86,6 @@ public class App {
         exercise2.marks();
 
         System.out.println("\nTask 6: Fröhliche Zahlen");
-        exercise2.happyNumbers();
+        exercise2.happyNumbers();*/
     }
 }
