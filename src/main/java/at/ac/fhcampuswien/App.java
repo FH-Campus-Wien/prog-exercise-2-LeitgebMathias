f2f28c919f59e1cac03256bdf08edc41c42ba998
LeitgebMathias <mathias.leitgeb@stud.fh-campuswien.ac.at>
2022-10-08 18:16:25 +0200

    All Tasks solved.

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -6,7 +6,7 @@ public class App {
 
     //todo Task 1
     public void largestNumber(){
-        // input your solution here
+
         Scanner scanner = new Scanner(System.in);
         System.out.print("Number 1: ");
         double MaxNumber = scanner.nextDouble();
@@ -31,7 +31,7 @@ public class App {
 
     //todo Task 2
     public void stairs(){
-        // input your solution here
+
         Scanner scanner = new Scanner(System.in);
         System.out.print("n: ");
         int NumberOfLines = scanner.nextInt();
@@ -50,13 +50,12 @@ public class App {
 
     //todo Task 3
     public void printPyramid(){
-        // input your solution here
+
         final int NumberOfLines = 6;
         StringBuilder StarString;
-        char space = ' ';
-        char star = '*';
-        // Die Anzahl der Leerzeichen wird mit jeder Zeile um eines Weniger.
-        // Die Sterne werden mit jeder Zeile um 2 mehr.
+        final char space = ' ';
+        final char star = '*';
+
         for(int i=1 ; i<=NumberOfLines ; i++){
             StarString = new StringBuilder();
             // Es wird 1 zu j dazu addiert, weil i mit 1 startet.
@@ -72,28 +71,23 @@ public class App {
 
     //todo Task 4
     public void printRhombus(){
-        // input your solution here
 
-        // Enclose possible Char Range to a optically visible one.
+        // Bereich eingrenzen, in dem alle Chars auch wirklich sichtbar sind.
+        Scanner scanner = new Scanner(System.in);
         final char firstAllowedAsciiSignForRhombus  = 33;   // = !
         final char lastAllowedAsciiSignForRhombus   = 126;  // = ~
-
         final char space                            = 32;
 
-        Scanner scanner = new Scanner(System.in);
         System.out.print("h: ");
         int rhombusHeight = scanner.nextInt();
 
-        // Half of Rhombus Height is necessary for following loops.
-        int rhombusHeightHalfForLoop = (rhombusHeight / 2) + 1;
+        int rhombusHeightHalf = (rhombusHeight / 2) + 1;
 
         System.out.print("c: ");
         char rhombusChar = scanner.next().charAt(0);
-        // Controlling, if Chars that are going to be used, are in defined Char-Range.
-            // rhombusHalfForLoops get subtracted by 1, because
-            // the minimal Char that will be used is
-                // rhombusChar - the "rounded down" half of the rhombusheight.
-        if((rhombusChar - (rhombusHeightHalfForLoop - 1)) < firstAllowedAsciiSignForRhombus ||
+
+        // Kontrolle, ob alle anzuzeigenden Chars im "sichtbaren Bereich" sind.
+        if((rhombusChar - (rhombusHeightHalf - 1)) < firstAllowedAsciiSignForRhombus ||
                 (rhombusChar > lastAllowedAsciiSignForRhombus )){
             System.out.println("Invalid character Input!");
             return;
@@ -105,23 +99,14 @@ public class App {
         }
 
         boolean HalfRhombusHeightReached = false;
-        // "currentHorizontalLineValue" wird durchgehend hochgezählt.
-            // Sobald "currentHorizontalLineValue" allerdings über der Hälfte der Höhe vom Rhombus ist, wird
-            // "currentHorizontalLineValue" bei jedem Schleifendurchlauf um 1 reduziert ( + 1 -2 = -1).
-                // Dadurch kann es überhaupt "0" werden.
         for(int currentHorizontalLineValue = 1 ; currentHorizontalLineValue > 0 ; currentHorizontalLineValue++){
             StringBuilder CharString = new StringBuilder();
 
             // Die Anzahl der benötigten Leerzeichen ist indirekt proportional vom "currentHorizontalLineValue"
-            // Deshalb wird der Aktuell "Zeilenwert" von der Hälfte der Rhombushöhe abgezogen
-            for (int numberOfLeadingSpaces = rhombusHeightHalfForLoop - currentHorizontalLineValue; numberOfLeadingSpaces > 0; numberOfLeadingSpaces--){
+            for (int numberOfLeadingSpaces = rhombusHeightHalf - currentHorizontalLineValue; numberOfLeadingSpaces > 0; numberOfLeadingSpaces--){
                 CharString.append(space);
             }
 
-            // Für die Schleife bezüglich der notwendigen Zeichen wurde das gleiche Prinzip, wie bei der Rhombushöhe verwendet.
-                // In jeder Zeile wird "CurrentVerticalPositionValue" hochgezählt, bis die Vertikale Mitte des Rhombus überschritten ist.
-                    // Ab dann wird "CurrentVerticalPositionValue" mit jedem Durchlauf um 1 reduziert ( + 1 -2 = -1), bis
-                    // es letztendlich 0 ist.
             boolean halfRhombusWidthReached = false;
             for (int CurrentVerticalPositionValue = 1 ; CurrentVerticalPositionValue > 0 ; CurrentVerticalPositionValue++){
                 // Die Berechnung des jeweils zu verwendenden Chars ergibt sich aus
@@ -129,54 +114,114 @@ public class App {
                     // Wobei mit steigendem "currentHorizontalLineValue" ein kleineres Char als "Start" in der Zeile verwendet werden muss
                     // und abhängig vom CurrentVerticalPositionValue, ein Char mit höherer Wertigkeit verwendet werden soll.
                 char currentChar = (char) (rhombusChar - currentHorizontalLineValue + CurrentVerticalPositionValue);
-
                 CharString.append(currentChar);
 
-                // Abfrage, ob die Vertikale mitte des Rhombus schon erreicht wurde.
-                    // Die "Mitte" der auszugebenden Zeichen ist hier glücklicherweise immer auch genau "currentHorizontalLineValue"
-                if (CurrentVerticalPositionValue == (currentHorizontalLineValue)) halfRhombusWidthReached = true;
-
-                // Sobald die "Vertikale Mitte vom Rhombus" einmal erreicht wurde, wird die Laufvariable bei jedem Durchlauf reduziert, anstatt erhöht zu werden.
-                if (halfRhombusWidthReached) CurrentVerticalPositionValue = CurrentVerticalPositionValue - 2;
+               if (CurrentVerticalPositionValue == (currentHorizontalLineValue)) halfRhombusWidthReached = true;
+               if (halfRhombusWidthReached) CurrentVerticalPositionValue = CurrentVerticalPositionValue - 2;
             }
             System.out.println(CharString);
 
-            // Abfrage, ob die Horizontale Mitte des Rhombus schon erreicht wurde.
-            if(currentHorizontalLineValue==rhombusHeightHalfForLoop) HalfRhombusHeightReached = true;
-            // Sobald die horizontale Mitte erreicht wurde, wird die Laufvariable bei jedem Durchlauf reduziert, anstatt erhöht zu werden.
+            if(currentHorizontalLineValue==rhombusHeightHalf) HalfRhombusHeightReached = true;
             if (HalfRhombusHeightReached) currentHorizontalLineValue = currentHorizontalLineValue-2;
         }
     }
 
     //todo Task 5
     public void marks(){
-        // input your solution here
+
+        Scanner scanner = new Scanner(System.in);
+        int i=0;
+        double sum = 0;
+        int numberOfNegativeMarks = 0;
+
+        boolean zeroHasBeenEntered = false;
+        do{
+            int currentValue = scanner.nextInt();
+            System.out.print("Mark " + (i + 1) + ": ");
+            switch (currentValue){
+                case 0 : {
+                    zeroHasBeenEntered = true;
+                    i--;
+                    break;
+                }
+                case 1 : {
+                    sum += 1;
+                    break;
+                }
+                case 2 : {
+                    sum += 2;
+                    break;
+                }
+                case 3 : {
+                    sum += 3;
+                    break;
+                }
+                case 4 : {
+                    sum += 4;
+                    break;
+                }
+                case 5 : {
+                    sum += 5;
+                    numberOfNegativeMarks++;
+                    break;
+                }
+                default: {
+                    System.out.println("Invalid mark!");
+                    i--;
+                    break;
+                }
+            }
+            i++;
+        }while(!zeroHasBeenEntered);
+        if (i == 0) i = 1;
+        double average = sum / i;
+        System.out.println("Average: " + String.format("%.2f",average));
+        System.out.println("Negative marks: " + numberOfNegativeMarks);
+
     }
 
     //todo Task 6
     public void happyNumbers(){
-        // input your solution here
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("n: ");
+        long numberToCheck = scanner.nextLong();
+        long iteratedNumber;
+        final int resultOfSadNumber = 4;
+        final int resultOfHappyNumber = 1;
+
+        do {
+            iteratedNumber = 0;
+            while(numberToCheck != 0){
+                iteratedNumber += (long) Math.pow(numberToCheck % 10,2);
+                numberToCheck /= 10;
+            }
+            numberToCheck = iteratedNumber;
+        }while(iteratedNumber != resultOfSadNumber && iteratedNumber != resultOfHappyNumber);
+        if (iteratedNumber == resultOfHappyNumber) System.out.println("Happy number!");
+        else System.out.println("Sad number!");
     }
 
     public static void main(String[] args){
         App exercise2 = new App();
 
-        //System.out.println("Task 1: Largest Number");
-        //exercise2.largestNumber();
+        System.out.println("Task 1: Largest Number");
+        exercise2.largestNumber();
 
-        //System.out.println("\nTask 2: Stairs");
-        //exercise2.stairs();
+        System.out.println("\nTask 2: Stairs");
+        exercise2.stairs();
 
-        //System.out.println("\nTask 3: Pyramide");
-        //exercise2.printPyramid();
+        System.out.println("\nTask 3: Pyramide");
+        exercise2.printPyramid();
 
         System.out.println("\nTask 4: Raute");
         exercise2.printRhombus();
 
-        //System.out.println("\nTask 5: Notendurchschnitt");
-        //exercise2.marks();
+        System.out.println("\nTask 5: Notendurchschnitt");
+        exercise2.marks();
 
-        //System.out.println("\nTask 6: Fröhliche Zahlen");
-        //exercise2.happyNumbers();*/
+        System.out.println("\nTask 6: Fröhliche Zahlen");
+        exercise2.happyNumbers();
     }
 }
