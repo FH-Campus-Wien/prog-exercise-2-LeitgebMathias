8b04d67e2f0c19be0256b056e08172fc5eaaa477
LeitgebMathias <mathias.leitgeb@stud.fh-campuswien.ac.at>
2022-10-08 13:36:52 +0200

    Problem 4 : Gelöst.

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -74,70 +74,78 @@ public class App {
     public void printRhombus(){
         // input your solution here
 
-        // Originalversion
-            // Allgemeine Gedanken zur Übung :
-                // Ich mache alles in einer Schleife.
-                    // Die Schleife beginnt bei 1 und die Laufbedingung ist, dass die Zahl größer 0 ist.
-                    // Was ist das obere Ende?
-                        // Der (Eingabewert in INT : 2) + 1.
-                            // Mit einer If- Bedingung frage ich ab, ob das erreicht wurde und setzte eine Boolean.
-                    // Die Boolean, welche gesetzt wird, wird am Ende der Schleife abgefragt
-                        // Wenn diese $true ist, dann ziehe ich von der Laufvariable 2 ab.
-            /*  Originalversion
-                Scanner scanner = new Scanner(System.in);
-                System.out.print("h:");
-                int rhombusHeight = scanner.nextInt();
-                if(rhombusHeight % 2 == 0){
-                    System.out.println("Invalid number!");
-                    return;
-                }
-                boolean HalfRhombusHeightReached = false;
-                int rhombusHeightHalfForLoop = rhombusHeight / 2 + 1;
-                for(int i = 1 ; i > 0 ; i++){
+        // Enclose possible Char Range to a optically visible one.
+        final char firstAllowedAsciiSignForRhombus  = 33;   // = !
+        final char lastAllowedAsciiSignForRhombus   = 126;  // = ~
 
-                    //noinspection IfStatementMissingBreakInLoop
-                    if(i==rhombusHeightHalfForLoop) HalfRhombusHeightReached = true;
-                    if (HalfRhombusHeightReached) i = i-2;
+        final char space                            = 32;
 
-                }
-            */
-        // Testcase mit der Doppelseitigen variablen Pyramide :
-        //
         Scanner scanner = new Scanner(System.in);
-        System.out.print("h:");
+        System.out.print("h: ");
         int rhombusHeight = scanner.nextInt();
+
+        // Half of Rhombus Height is necessary for following loops.
+        int rhombusHeightHalfForLoop = (rhombusHeight / 2) + 1;
+
+        System.out.print("c: ");
+        char rhombusChar = scanner.next().charAt(0);
+        // Controlling, if Chars that are going to be used, are in defined Char-Range.
+            // rhombusHalfForLoops get subtracted by 1, because
+            // the minimal Char that will be used is
+                // rhombusChar - the "rounded down" half of the rhombusheight.
+        if((rhombusChar - (rhombusHeightHalfForLoop - 1)) < firstAllowedAsciiSignForRhombus ||
+                (rhombusChar > lastAllowedAsciiSignForRhombus )){
+            System.out.println("Invalid character Input!");
+            return;
+        }
+
         if(rhombusHeight % 2 == 0){
             System.out.println("Invalid number!");
             return;
         }
 
-        StringBuilder StarString;
-        char space = ' ';
-        char star = '*';
-
         boolean HalfRhombusHeightReached = false;
-        int rhombusHeightHalfForLoop = rhombusHeight / 2 + 1;
+        // "currentHorizontalLineValue" wird durchgehend hochgezählt.
+            // Sobald "currentHorizontalLineValue" allerdings über der Hälfte der Höhe vom Rhombus ist, wird
+            // "currentHorizontalLineValue" bei jedem Schleifendurchlauf um 1 reduziert ( + 1 -2 = -1).
+                // Dadurch kann es überhaupt "0" werden.
+        for(int currentHorizontalLineValue = 1 ; currentHorizontalLineValue > 0 ; currentHorizontalLineValue++){
+            StringBuilder CharString = new StringBuilder();
 
-        for(int currentHorizontalLine = 1 ; currentHorizontalLine > 0 ; currentHorizontalLine++){
-            StarString = new StringBuilder();
-            // Passt, weil ich für die Leerzeichen eine Nummer weniger als, die aufgerundete Hälfte brauche.
-            // Gleichzeitig startet currentHorizontalLine mit 1
-            for (int numberOfLeadingSpaces = rhombusHeightHalfForLoop - currentHorizontalLine; numberOfLeadingSpaces > 0; numberOfLeadingSpaces--){
-                StarString.append(space);
+            // Die Anzahl der benötigten Leerzeichen ist indirekt proportional vom "currentHorizontalLineValue"
+            // Deshalb wird der Aktuell "Zeilenwert" von der Hälfte der Rhombushöhe abgezogen
+            for (int numberOfLeadingSpaces = rhombusHeightHalfForLoop - currentHorizontalLineValue; numberOfLeadingSpaces > 0; numberOfLeadingSpaces--){
+                CharString.append(space);
             }
-            for (int numberOfStars = 0 ; numberOfStars < 2*currentHorizontalLine -1 ; numberOfStars++){
-                // Das ist jetzt die Schleife, in der ich mich um die richtigen Zeichen kümmern muss!
 
+            // Für die Schleife bezüglich der notwendigen Zeichen wurde das gleiche Prinzip, wie bei der Rhombushöhe verwendet.
+                // In jeder Zeile wird "CurrentVerticalPositionValue" hochgezählt, bis die Vertikale Mitte des Rhombus überschritten ist.
+                    // Ab dann wird "CurrentVerticalPositionValue" mit jedem Durchlauf um 1 reduziert ( + 1 -2 = -1), bis
+                    // es letztendlich 0 ist.
+            boolean halfRhombusWidthReached = false;
+            for (int CurrentVerticalPositionValue = 1 ; CurrentVerticalPositionValue > 0 ; CurrentVerticalPositionValue++){
+                // Die Berechnung des jeweils zu verwendenden Chars ergibt sich aus
+                    // Dem anfangs eingegebenen Char.
+                    // Wobei mit steigendem "currentHorizontalLineValue" ein kleineres Char als "Start" in der Zeile verwendet werden muss
+                    // und abhängig vom CurrentVerticalPositionValue, ein Char mit höherer Wertigkeit verwendet werden soll.
+                char currentChar = (char) (rhombusChar - currentHorizontalLineValue + CurrentVerticalPositionValue);
 
-                StarString.append(star);
+                CharString.append(currentChar);
+
+                // Abfrage, ob die Vertikale mitte des Rhombus schon erreicht wurde.
+                    // Die "Mitte" der auszugebenden Zeichen ist hier glücklicherweise immer auch genau "currentHorizontalLineValue"
+                if (CurrentVerticalPositionValue == (currentHorizontalLineValue)) halfRhombusWidthReached = true;
+
+                // Sobald die "Vertikale Mitte vom Rhombus" einmal erreicht wurde, wird die Laufvariable bei jedem Durchlauf reduziert, anstatt erhöht zu werden.
+                if (halfRhombusWidthReached) CurrentVerticalPositionValue = CurrentVerticalPositionValue - 2;
             }
-            System.out.println(StarString);
-            
-            if(currentHorizontalLine==rhombusHeightHalfForLoop) HalfRhombusHeightReached = true;
-            if (HalfRhombusHeightReached) currentHorizontalLine = currentHorizontalLine-2;
+            System.out.println(CharString);
+
+            // Abfrage, ob die Horizontale Mitte des Rhombus schon erreicht wurde.
+            if(currentHorizontalLineValue==rhombusHeightHalfForLoop) HalfRhombusHeightReached = true;
+            // Sobald die horizontale Mitte erreicht wurde, wird die Laufvariable bei jedem Durchlauf reduziert, anstatt erhöht zu werden.
+            if (HalfRhombusHeightReached) currentHorizontalLineValue = currentHorizontalLineValue-2;
         }
-
-
     }
 
     //todo Task 5
